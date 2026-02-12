b02609c136c2d326e436dfa1df444ac992dc6d8d
Alfred Chin <alfredchin2005@gmail.com>
2026-02-12 16:13:51 +0800

    Remove previous user modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
-import { UserModule } from './user/user.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
