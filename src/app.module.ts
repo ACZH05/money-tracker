da19e57402697338f0b6f357d1a9bc67edd65413
Alfred Chin <alfredchin2005@gmail.com>
2026-02-15 19:54:07 +0800

    Change format to use single quote

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller.js";
-import { AppService } from "./app.service.js";
-import { PrismaService } from "./prisma/prisma.service.js";
-import { ConfigModule } from "@nestjs/config";
-import { UserModule } from "./user/user.module.js";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+import { PrismaService } from './prisma/prisma.service.js';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module.js';
+import { AuthModule } from './auth/auth.module.js';
 
 @Module({
-  imports: [ConfigModule.forRoot({}), UserModule],
+  imports: [ConfigModule.forRoot({}), UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
