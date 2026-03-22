ba45e5558877da87ddf0eec1cbf9fdb16212a51c
ACZH05 <alfredchin2005@gmail.com>
2026-03-22 21:23:30 +0800

    Add find all transaction function

--- api/src/transaction/transaction.controller.ts
+++ api/src/transaction/transaction.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TransactionService } from './transaction.service.js';
 
 @Controller('transaction')
 export class TransactionController {
   constructor(private readonly transactionService: TransactionService) {}
+
+  @Get()
+  async getAllTransaction() {
+    return this.transactionService.findAll();
+  }
 }
--- api/src/transaction/transaction.service.ts
+++ api/src/transaction/transaction.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Transaction } from '../../generated/prisma/client.js';
+import { prisma } from '../prisma/prisma.export.js';
 
 @Injectable()
-export class TransactionService {}
+export class TransactionService {
+  async findAll(): Promise<Transaction[]> {
+    return prisma.transaction.findMany();
+  }
+}
