23aa997caa2a28f2d01f0353c17c8a5464118e96
ACZH05 <alfredchin2005@gmail.com>
2026-03-22 17:43:54 +0800

    Add transaction route

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,9 +5,15 @@ import { PrismaService } from './prisma/prisma.service.js';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module.js';
 import { AuthModule } from './auth/auth.module.js';
+import { TransactionModule } from './transaction/transaction.module.js';
 
 @Module({
-  imports: [ConfigModule.forRoot({}), UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({}),
+    UserModule,
+    AuthModule,
+    TransactionModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
